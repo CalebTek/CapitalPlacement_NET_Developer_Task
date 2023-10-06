146d902b00e74a0ce9c9f3b46865136019f5056f
CalebTek <walexjackson@gmail.com>
2023-10-06 18:28:06 +0100

    Created Enums Helper

---
+++ CapitalPlacement.Domain/Enums/Helper/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CapitalPlacement.Domain.Enums.Helper
+{
+    public class EnumHelper
+    {
+        public static string GetEnumDescription<TEnum>(TEnum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+            if (fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+            {
+                return attribute.Description;
+            }
+
+            return value.ToString();
+        }
+    }
+}
