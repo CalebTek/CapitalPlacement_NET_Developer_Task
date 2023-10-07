2b1c6eee63633efe7a2621039a6d089b978f1fbf
CalebTek <olawale.odeyemi@decagon.dev>
2023-10-07 04:15:13 +0100

    Added Application Form Repository Service (Interface and Implementation)

---
+++ CapitalPlacement.Infrastructure/Implementation/ApplicationFormRepository.cs
@@ -0,0 +1,71 @@
+﻿using CapitalPlacement.Domain.Data;
+using CapitalPlacement.Domain.Models;
+using CapitalPlacement.Infrastructure.Interface;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.Infrastructure.Implementation
+{
+    public class ApplicationFormRepository : IApplicationFormRepository
+    {
+        private readonly DataAccess _dataAccess;
+        private readonly string _containerId = "applicationforms"; 
+
+        public ApplicationFormRepository(DataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<ApplicationForm> CreateApplicationFormAsync(ApplicationForm applicationForm)
+        {
+            try
+            {
+                ItemResponse<ApplicationForm> response = await _dataAccess.CreateItemAsync(applicationForm, _containerId);
+                return response.Resource;
+            }
+            catch (CosmosException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<ApplicationForm> GetApplicationFormAsync(string applicationFormId)
+        {
+            try
+            {
+                ItemResponse<ApplicationForm> response = await _dataAccess.ReadItemAsync<ApplicationForm>(applicationFormId, _containerId);
+                return response.Resource;
+            }
+            catch (CosmosException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<ApplicationForm> UpdateApplicationFormAsync(ApplicationForm applicationForm)
+        {
+            try
+            {
+                ItemResponse<ApplicationForm> response = await _dataAccess.UpsertItemAsync(applicationForm, _containerId);
+                return response.Resource;
+            }
+            catch (CosmosException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task DeleteApplicationFormAsync(string applicationFormId)
+        {
+            try
+            {
+                await _dataAccess.DeleteItemAsync<ApplicationForm>(applicationFormId, _containerId);
+            }
+            catch (CosmosException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CapitalPlacement.Infrastructure/Interface/IApplicationFormRepository.cs
@@ -0,0 +1,17 @@
+﻿using CapitalPlacement.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalPlacement.Infrastructure.Interface
+{
+    public interface IApplicationFormRepository
+    {
+        Task<ApplicationForm> CreateApplicationFormAsync(ApplicationForm applicationForm);
+        Task<ApplicationForm> GetApplicationFormAsync(string applicationFormId);
+        Task<ApplicationForm> UpdateApplicationFormAsync(ApplicationForm applicationForm);
+        Task DeleteApplicationFormAsync(string applicationFormId);
+    }
+}
